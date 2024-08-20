f550408949ea81a5b9c1a6c763eaca64eac699cf
mic050r <s2216@e-mirim.hs.kr>
2024-08-20 15:28:14 +0900

    FEAT(#2) :: WebSocket 설정 추가

---
+++ src/main/java/kr/hs/study/springboot_chat/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package kr.hs.study.springboot_chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // 메시지를 브로커로 라우팅하는 경로 설정
+        config.enableSimpleBroker("/topic");
+        // 클라이언트가 메시지를 보내는 경로 설정
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // WebSocket 연결 엔드포인트 설정
+        registry.addEndpoint("/ws").withSockJS();
+    }
+}
