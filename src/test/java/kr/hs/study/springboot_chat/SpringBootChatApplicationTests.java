30c746fd90f9a5803c78789237dbc82fda021312
mic050r <s2216@e-mirim.hs.kr>
2024-08-20 11:48:01 +0900

    init commit

---
+++ src/test/java/kr/hs/study/springboot_chat/SpringBootChatApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.study.springboot_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
